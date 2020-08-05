774c34440a0bf36f09fd10ec7ba0ebe19c830d60
JeSuisVictoria <53588237+Vicky0603@users.noreply.github.com>
2020-08-04 23:52:12 -0300

    Add files via upload

---
+++ LightVersion.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: LightVersion
+Description: Adding buttons of differents services like VK, FB and etc
+*/
+
+// adding filter
+add_filter( 'the_content', 'yandexshare_run' );
+
+// functions adds buttons before a post
+function LightVersion_run($content) {
+    // code from the page https://tech.yandex.ru/share/
+    $data = '
+        <script type="text/javascript" src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js" charset="utf-8"></script>
+        <script type="text/javascript" src="//yastatic.net/share2/share.js" charset="utf-8"></script>
+        <div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,gplus,twitter"></div>
+    ';
+    
+    // return the old and new data
+    return $data . $content;
+}
